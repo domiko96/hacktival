813c672d156f3f7fb802ee234888cfcba45017b6
domiko96 <domiko96@gmail.com>
2019-05-11 23:38:41 +0200

    vis

--- python Interface/vis.py
+++ python Interface/vis.py
@@ -5,7 +5,7 @@ from PIL import Image
 ser = serial.Serial('/dev/ttyACM0')  # open serial port
 ser.baudrate = 115200
 red = "0"
-green = "255"
+green = "10"
 blue = "0"
 red = red.zfill(3)
 green = green.zfill(3)
@@ -25,31 +25,33 @@ def draw(r,g,b):
 
 
 TARGET_HEIGHT = 118
-SOURCE_IMAGE = "test2.jpeg"
+SOURCE_IMAGE = "hacktival.bmp"
 
 sample_img = Image.open(SOURCE_IMAGE)
 
 orig_width, orig_height = sample_img.size
 target_width = orig_width * TARGET_HEIGHT // orig_height
 
-target_img = sample_img.resize((target_width, TARGET_HEIGHT), Image.BICUBIC, None)
+target_img = sample_img.resize((target_width, TARGET_HEIGHT), Image.NEAREST, None)
 print("Image Collumns: ", target_width)
 
 target_img.save("target.jpg")
 rgb_data = target_img.convert('RGB');
 
 #target_data = []
-#for col in range(0, target_width - 1):
+for col in range(0, target_width - 1):
 #    print("row")
 #    print(col)
-#    for row in range(TARGET_HEIGHT - 1, -1, -1):
-#        r, g, b = target_img.getpixel((col, row))
-#        draw(str(r).zfill(3),str(g).zfill(3),str(b).zfill(3))
-#    time.sleep(0.25)
-#    for row in range(TARGET_HEIGHT):
-#        draw(str(0).zfill(3),str(0).zfill(3),str(0).zfill(3))
-#        pass
-#    time.sleep(1)
+    for row in range(TARGET_HEIGHT - 1, -1, -1):
+        r, g, b = target_img.getpixel((col, row))
+        print(str(r).zfill(3),str(g).zfill(3),str(b).zfill(3))
+        draw(str(r).zfill(3),str(g).zfill(3),str(b).zfill(3))
+    #time.sleep(0.25)
+    #print("-------------------------")
+    #for row in range(TARGET_HEIGHT):
+    #    draw(str(0).zfill(3),str(0).zfill(3),str(0).zfill(3))
+    #    pass
+    raw_input("Press Enter to continue...")
 
 
 #curr_col.append([str(r).zfill(3), str(g).zfill(3), str(b).zfill(3)])
@@ -57,30 +59,30 @@ rgb_data = target_img.convert('RGB');
 #print(target_data)
 
 
-print(ser.name)         # check which port was really used
-for value in range(118):
-    #print("LED-Nr:")
-    #print(value)
-    draw(red,green,blue)
-    pass
-print("sent data")
+# print(ser.name)         # check which port was really used
+# for value in range(118):
+#     #print("LED-Nr:")
+#     #print(value)
+#     draw(red,green,blue)
+#     pass
+# print("sent data")
 
-time.sleep(1)
+# time.sleep(0)
 
-red = "255"
-green = "0"
-blue = "0"
-red = red.zfill(3)
-green = green.zfill(3)
-blue = blue.zfill(3)
+# red = "0"
+# green = "0"
+# blue = "0"
+# red = red.zfill(3)
+# green = green.zfill(3)
+# blue = blue.zfill(3)
 
-for value in range(118):
-    #print("LED-Nr:")
-    #print(value)
-    draw(red,green,blue)
-    pass
-print("sent data")
+# for value in range(118):
+#     #print("LED-Nr:")
+#     #print(value)
+#     draw(red,green,blue)
+#     pass
+# print("sent data")
 
-time.sleep(1)
+# time.sleep(1)
 
 ser.close()
