a4e1780418a732522f93ad75931a1dfb5ccf4c2b
domiko96 <domiko96@gmail.com>
2019-05-11 23:14:58 +0200

    cur

--- firmware/hacktival/src/main.cpp
+++ firmware/hacktival/src/main.cpp
@@ -1,58 +1,67 @@
 #include <FastLED.h>
 
 // How many leds in your strip?
-#define NUM_LEDS 117
+#define NUM_LEDS 118
 // For led chips like Neopixels, which have a data line, ground, and power, you just
 // need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
 // ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
 #define DATA_PIN 11
 //#define CLOCK_PIN 13
 
+int rValue = 0;
+int gValue = 0;
+int bValue = 0;
+
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
 void setup() {
-      // Uncomment/edit one of the following lines for your leds arrangement.
-      // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  	  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
-
-      // FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<P9813, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA102, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN>(leds, NUM_LEDS);
-      // FastLED.addLeds<DOTSTAR, RGB>(leds, NUM_LEDS);
-
-      // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
-      // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+      FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+      Serial.begin(115200);
 }
 
 void loop() {
-  // Turn the LED on, then pause
-  for (size_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Red;
-    FastLED.show();
-    delay(800);
-  }
 
-  for (size_t i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Black;
-    FastLED.show();
-    delay(800);
+  size_t i = 0;
+  while (i < NUM_LEDS) {
+    if (Serial.available() >= 11) {
+              // read the incoming byte:
+              rValue = Serial.parseInt();
+              gValue = Serial.parseInt();
+              bValue = Serial.parseInt();
+
+              // say what you got:
+              Serial.print("I received: ");
+              Serial.print(rValue);
+              Serial.print(gValue);
+              Serial.print(bValue);
+
+              leds[i].r = rValue;
+              leds[i].g = gValue;
+              leds[i].b = bValue;
+               //go to next line
+              i++;
+
+      }
   }
+  FastLED.show();
+  Serial.println("Called show");
+  i=0;
+
+
+
+
+
+  // Turn the LED on, then pause
+  //for (size_t i = 0; i < NUM_LEDS; i++) {
+  //  leds[i] = CRGB::Red;
+  //  FastLED.show();
+  //  delay(10);
+  //}
+
+  //for (size_t i = NUM_LEDS; i != 0; i--) {
+  //  leds[i] = CRGB::Black;
+  //  FastLED.show();
+  //  delay(10);
+  //}
 }
